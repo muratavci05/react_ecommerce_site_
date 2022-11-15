a505457268c406f2a0f854ddb6b76d4b385a3841
muratavci05 <js.muratavci@gmail.com>
2022-11-15 09:59:57 +0300

    cart Reducer add

--- src/Redux/Reducer/cartReducer.js
+++ src/Redux/Reducer/cartReducer.js
@@ -2,22 +2,32 @@ const initialState = null
 
 export const SET_CART = 'set_cart'
 export const REMOVE_CART = 'remove_cart'
-export const SET_CART_ITEMS = 'set_cart_items'
+// set_cart ile set_cart_item verileri aynı olduğu için set cart items'i kaldırıyorum
+//export const SET_CART_ITEMS = 'set_cart_items' 
 
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_CART:
-      return action.payload
+      const stateData={
+            ...action.payload,
+            items: action.payload.items,
+            itemsCount: action.payload.items.length,
+      }
+      localStorage.setItem("cart",jSON.stringify(stateData));
+        
+      return stateData
+      
 
     case REMOVE_CART:
+        localStorage.removeItem("cart");
       return null
 
-    case SET_CART_ITEMS:
+    /* case SET_CART_ITEMS:
       return {
         ...state,
         items: action.payload.items,
         itemsTotal: action.payload.items.length,
-      }
+      } */
 
     default:
       return state
